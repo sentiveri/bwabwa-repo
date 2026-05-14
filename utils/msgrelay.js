eb54e77fcddec462ec0b3b9b1f1853f666e1765a
sentiveri <commentchua1@gmail.com>
2026-05-14 17:20:54 +0700

    util msg relay for some reason

--- utils/msgrelay.js
+++ utils/msgrelay.js
@@ -1,32 +1,55 @@
 module.exports = async (message) => {
 
-  if (!message.client.relayEnabled) return;
+  console.log("MESSAGE DETECTED");
+
+  if (!message.client.relayEnabled) {
+    console.log("Relay disabled");
+    return;
+  }
 
   const SOURCE_CHANNEL =
     process.env.SOURCE_CHANNEL;
+
+  console.log("Channel:", message.channel.id);
+  console.log("Source:", SOURCE_CHANNEL);
 
   const TARGET_CHANNELS = [
     process.env.TARGET1,
   ];
 
-  if (message.author.id === message.client.user.id) return;
+  if (message.author.id === message.client.user.id) {
+    console.log("Own bot message ignored");
+    return;
+  }
 
-  if (message.channel.id !== SOURCE_CHANNEL) return;
+  if (message.channel.id !== SOURCE_CHANNEL) {
+    console.log("Wrong source channel");
+    return;
+  }
+
+  console.log("Passing checks");
 
   for (const channelId of TARGET_CHANNELS) {
 
     try {
 
+      console.log("Sending to:", channelId);
+
       const channel =
         await message.client.channels.fetch(channelId);
 
-      if (!channel) continue;
+      if (!channel) {
+        console.log("Channel not found");
+        continue;
+      }
 
       await channel.send({
         content: message.content || null,
         embeds: message.embeds,
         files: [...message.attachments.values()]
       });
+
+      console.log("Sent successfully");
 
     } catch (err) {
       console.log(err);
