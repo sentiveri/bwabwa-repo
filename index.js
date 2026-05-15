4ddf299ba76cc62bf3d5ae554bb909f8325e4ef3
sentiveri <commentchua1@gmail.com>
2026-05-15 13:50:24 +0700

    hotfix

--- index.js
+++ index.js
@@ -126,7 +126,8 @@ client.on(Events.InteractionCreate, async interaction => {
 });
 
 client.on(Events.MessageCreate, async message => {
-  if (message.author.bot && !message.webhookId) return;
+  if (message.author.bot) return;
+  if (message.webhookId && message.channel.id !== process.env.SOURCE_CHANNEL) return;
 
   if (client.relayEnabled) {
     if (message.channel.id === process.env.SOURCE1) {
